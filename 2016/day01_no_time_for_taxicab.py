3145044818addfa1a37760f1b1d5a0f54a452a0e
falk <falkdavid@gmx.de>
2019-11-10 11:07:56 +0100

    Initial commit

---
+++ 2016/day01_no_time_for_taxicab.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+    - Run the script in an ipython session using '%run <name>'
+    - Make sure to 'export AOC_SESSION=<your_aoc_token>'
+"""
+
+from aocd.models import Puzzle
+from typing import List
+
+puzzle = Puzzle(2016, 1)
+
+data = puzzle.input_data
+directions = data.split(", ")
+
+######################################################################
+#   Part 1
+######################################################################
+
+class Coord(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return f"Coord({self.x}, {self.y})"
+
+# State machine to keep track of direction to save a bunch of ifs
+alph = "RL"
+fsm = {'N':"EW", 'E':"SN", 'S':"WE", 'W':"NS"}
+axis = {'x':{'E':1,'W':-1}, 'y':{'N':1,'S':-1}}
+
+
+def follow_directions(directions: List[str], start_facing: str = 'N') -> Coord:
+    pos = Coord(0, 0)
+    facing = start_facing
+
+    for d in directions:
+        turn = alph.index(d[0])
+        amount = int(d[1:])
+
+        # turn around
+        facing = fsm[facing][turn]
+        # move
+        if facing in axis['x']:
+            pos.x += axis['x'][facing] * amount
+        else:
+            pos.y += axis['y'][facing] * amount
+        
+    return pos
+
+def get_distance(directions: List[str]) -> int:
+    end = follow_directions(directions)
+    return abs(end.x) + abs(end.y)
+
+assert get_distance(["R2", "L3"]) == 5
+assert get_distance(["R2", "R2", "R2"]) == 2
+assert get_distance(["R5", "L5", "R5", "R3"]) == 12
+
+print(get_distance(directions))
+
+# puzzle.awnser_a = get_distance(directions)
+
+
+######################################################################
+#   Part 2
+######################################################################
+
+def visit_twice(directions: List[str], start_facing: str = 'N') -> Coord:
+    current_pos = Coord(0, 0)
+    facing = start_facing
+
+    seen_pos = {(0,0)}
+
+    for d in directions:
+        turn = alph.index(d[0])
+        amount = int(d[1:])
+
+        facing = fsm[facing][turn]
+        if facing in axis['x']:
+            new_pos = [(current_pos.x + axis['x'][facing]*a, current_pos.y)
+                        for a in range(1, amount + 1)]
+            
+            if seen_pos.intersection(set(new_pos)):
+                return list(seen_pos.intersection(new_pos))[0]
+
+            seen_pos.update(new_pos)
+            current_pos.x += axis['x'][facing] * amount
+        else:
+            new_pos = [(current_pos.x, current_pos.y + axis['y'][facing]*a)
+                        for a in range(1, amount + 1)]
+
+            if seen_pos.intersection(set(new_pos)):
+                return list(seen_pos.intersection(new_pos))[0]
+
+            seen_pos.update(new_pos)
+            current_pos.y += axis['y'][facing] * amount
+
+def get_distance_seen_twice(directions: List[str]) -> int:
+    end = visit_twice(directions)
+    if end:
+        return abs(end[0]) + abs(end[1])
+
+assert get_distance_seen_twice(["R8", "R4", "R4", "R8"]) == 4
+
+print(get_distance_seen_twice(directions))
+
+# puzzle.answer_b = get_distance_seen_twice(directions)
---
+++ template.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    - Run the script in an ipython session using '%run <name>'
+    - Make sure to 'export AOC_SESSION=<your_aoc_token>'
+"""
+
+from aocd.models import Puzzle
+
+
+# Enter year and day
+puzzle = Puzzle(2016, 1)
+
+data = puzzle.input_data
+
+######################################################################
+#   Part 1
+######################################################################
+
+
+# puzzle.answer_a = 
+
+
+######################################################################
+#   Part 2
+######################################################################
+
+
+# puzzle.answer_b = 
